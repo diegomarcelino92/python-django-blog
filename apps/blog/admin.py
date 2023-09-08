c598ed2e930d06878ae78e7b4878bb780c58918a
diegomarcelino92 <ddmarcelino2014@gmail.com>
2023-09-08 20:17:59 -0300

    feat: adding models for page, tags and category

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+from apps.blog.models import Category, Page, Post, Tag
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'slug'
+    list_display_links = 'id',
+    search_fields = 'id', 'name', 'slug'
+    list_per_page = 10
+    ordering = '-id',
+    prepopulated_fields = {"slug": ('name',), }
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'slug'
+    list_display_links = 'id',
+    search_fields = 'id', 'name', 'slug'
+    list_per_page = 10
+    ordering = '-id',
+    prepopulated_fields = {"slug": ('name',), }
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title', 'is_published'
+    list_display_links = 'id',
+    search_fields = 'id', 'title', 'slug', 'content'
+    list_filter = 'is_published',
+    list_editable = 'is_published',
+    ordering = '-id',
+    list_per_page = 50
+    prepopulated_fields = {"slug": ('title',), }
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = 'content',
+    list_display = 'id', 'title', 'is_published', 'created_by'
+    list_display_links = 'id',
+    search_fields = 'id', 'title', 'slug', 'content', 'excertp', 'content',
+    list_filter = 'category', 'is_published',
+    list_editable = 'is_published',
+    readonly_fields = 'created_at', 'updated_at', 'created_by', 'updated_by',
+    ordering = '-id',
+    list_per_page = 50
+    prepopulated_fields = {"slug": ('title',), }
+
+    def save_model(self, req, model, _, change):
+        if change:
+            model.updated_by = req.user
+        else:
+            model.created_by = req.user
+
+        model.save()
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,1 +1,105 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+from apps.utils.files import resize_image, upload_to
+from apps.utils.rands import custom_slugify
+
+
+class Tag(models.Model):
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(
+        max_length=255, default=None,
+        null=True, unique=True
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = custom_slugify(self.name)
+
+        return super().save(*args, **kwargs)
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(
+        max_length=255, default=None,
+        null=True, unique=True
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = custom_slugify(self.name)
+
+        return super().save(*args, **kwargs)
+
+
+class Page(models.Model):
+    title = models.CharField(max_length=65)
+    slug = models.SlugField(
+        max_length=255, default=None,
+        null=True, unique=True
+
+    )
+    content = models.TextField()
+    is_published = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = custom_slugify(self.title)
+
+        return super().save(*args, **kwargs)
+
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    title = models.CharField(max_length=65)
+    slug = models.SlugField(
+        max_length=255, default=None,
+        null=True, unique=True
+    )
+    cover = models.ImageField(upload_to=upload_to, blank=True, default=None)
+    cover_content = models.BooleanField(default=True)
+    excerpt = models.CharField(max_length=150)
+    content = models.TextField()
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True,
+        blank=True, default=None
+    )
+    tags = models.ManyToManyField(Tag, blank=True, default='')
+    is_published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL,
+                                   null=True, blank=True, default=None,
+                                   related_name='page_created_by'
+                                   )
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(User, on_delete=models.SET_NULL,
+                                   null=True, blank=True, default=None,
+                                   related_name='page_updated_by'
+                                   )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = custom_slugify(self.title, 4)
+
+        current_cover = str(self.cover.name)
+        cover_changed = False
+
+        if self.cover:
+            cover_changed = current_cover != self.cover.name
+
+        if cover_changed:
+            resize_image(self.cover, 900)
+
+        return super().save(*args, **kwargs)
